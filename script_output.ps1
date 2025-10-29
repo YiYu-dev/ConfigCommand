5ff7d076d2bf6c254b2a1eb0020b99911317cc42
YiYu-dev <yuta.contact03@gmail.com>
2025-10-29 16:57:59 +0900

    Update script_output.ps1

--- script_output.ps1
+++ script_output.ps1
@@ -1,7 +1,7 @@
 $location = "japan east"
 $SubscriptionId = (az account show | convertfrom-json).id
 $tenantId = (az account show | convertfrom-json).tenantid
-$Id = "002"
+$Id = "003"
 $ResourceDefaultNm = "jp.rbs.wak-any"
 $ResourceDefaultNmhyphen = "jp-rbs-wak-any"
 $ResourceBaseNm = "$ResourceDefaultNm-$Id"
